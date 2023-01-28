8bce0172e22835cf8277b66668a9805161939844
Kavrese <kavrese@gmail.com>
2023-01-28 13:08:47 +0300

    configuration ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,12 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
+ktor {
+    fatJar {
+        archiveFileName.set("plants-api.jar")
+    }
+}
+
 repositories {
     mavenCentral()
 }
